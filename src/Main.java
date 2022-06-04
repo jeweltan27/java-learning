f4a0aef844d7586c6466de1259d01a63f4abaa74
jeweltan27 <jewelxyt@gmail.com>
2022-06-04 21:23:01 +0800

    Add new message again

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Good afternoon! I am going to exercise now");
+        System.out.println(4 + 5);
     }
 }
